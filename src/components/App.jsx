2e5473b92ed817b60015f0fbfc731fd702ed6694
Ev Bludov <120216330+ernestoDug@users.noreply.github.com>
2023-07-14 12:32:02 +0300

    6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 
 
 
-// import  Profile from 'components/Profile/Profile';
-// import user from 'user.json'
+// кОмпО
+import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
 
-
+// джейСини
+import user from 'user.json';
+import data from 'data.json';
+import friends from 'friends.json';
 
 
 
@@ -18,31 +23,34 @@ import React from 'react';
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        // display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
-    >
+    >,
+      <Profile 
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    />,
+ <Statistics 
+ title="Upload stats" 
+ stats={data} />,
+ 
+<FriendList friends={friends} />
     </div>
-  );
-};
+  )
+    }
+
--- src/index.js
+++ src/index.js
@@ -3,37 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-// кОмпО
-import Profile from 'components/Profile/Profile';
-import Statistics from 'components/Statistics/Statistics';
-import FriendList from 'components/FriendList/FriendList';
 
-// джейСини
-import user from 'user.json';
-import data from 'data.json';
-import friends from 'friends.json';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />,
-    <Profile 
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    />,
- <Statistics 
- title="Upload stats" 
- stats={data} />,
- 
-<FriendList friends={friends} />;
-
-  </React.StrictMode>
+   </React.StrictMode>
 );
 // console.log('fdfdsdsewe');
 
